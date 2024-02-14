e9f86b741c44e4ed4ac3ad89b498c4bbd6c6b6d5
Luz Noh <luznoh274@gmail.com>
2024-02-14 17:27:22 -0600

    S4/L05

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,15 +6,22 @@ void main()
 {
   runApp(MyApp());
 }
+
 //CLASE QUE SE VA INSASTIAR
 class MyApp extends StatelessWidget 
 {
+  //inicialización para la llave
+  const MyApp({super.key});
+
   //HAY QUE REGRESAR UN WIDGET
   @override
   Widget build(BuildContext context) 
   {
-    return MaterialApp(home:Center(child: Text('Hola Mundo')) ,);// Crea un text widget
+    return const MaterialApp // estamos diciendo que esto no va a cambiar
+    (
+       debugShowCheckedModeBanner: false,
+        home: Scaffold
+        (body: Center(child: Text('Hola Mundo')),)
+    ); // Crea un text widget
   }
-  
- 
 }
